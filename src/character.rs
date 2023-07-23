b5770de5571fb772db007a2362ace9a3d5781eea
Spencer Dent <mr.spencerdent@gmail.com>
2023-07-22 23:57:04 -0400

    feat: encapsulate `Classes`
    `Character` was starting to have too many responsibilities.
    Breaking `Classes` out into its own `struct` should help reduce the
    cognitive load when trying to work with `Character` (as well as reduce
    the headaches induced by testing...).

--- src/character.rs
+++ src/character.rs
@@ -1,6 +1,6 @@
 use crate::{
     ability::{Abilities, Ability},
-    class::Class,
+    class::Classes,
     modifiers::Proficiency,
     race::{CreatureType, Race, Size},
 };
@@ -41,7 +41,7 @@ pub struct Character {
     pub personality: Personality,
     race: Box<dyn Race>,
     abilities: Abilities,
-    classes: Vec<Box<dyn Class>>,
+    classes: Classes,
 }
 
 impl Character {
@@ -66,37 +66,25 @@ impl Character {
     }
 
     pub fn get_level(&self) -> usize {
-        self.classes
-            .iter()
-            .fold(0, |acc, class| acc + class.get_level())
+        self.classes.get_level()
     }
 
     pub fn get_proficiency_bonus(&self) -> usize {
-        (self.get_level() - 1) / 4 + 2
+        self.classes.get_proficiency_bonus()
     }
 
     pub fn get_saving_throw_proficiency(&self, ability: &Ability) -> Option<Proficiency> {
-        self.classes
-            .first()
-            .and_then(|primary_class| primary_class.get_saving_throw_proficiency(ability))
+        self.classes.get_saving_throw_proficiency(ability)
     }
 
     pub fn get_saving_throw_mod(&self, ability: &Ability) -> isize {
-        self.get_proficiency_bonus() as isize
-            * self
-                .get_saving_throw_proficiency(ability)
-                .map(|p| p as isize)
-                .unwrap_or(0)
-            + self.get_ability_modifier(ability)
+        self.classes.get_saving_throw_bonus(ability) as isize + self.get_ability_modifier(ability)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{
-        class::{Artificer, Wizard},
-        race::Human,
-    };
+    use crate::{class::Artificer, race::Human};
 
     use super::*;
 
@@ -108,7 +96,7 @@ mod tests {
                 gender: None,
                 abilities: Abilities::default(),
                 race: Box::<Human>::default(),
-                classes: vec![],
+                classes: Classes::empty(),
                 personality: Personality {
                     personality_traits: vec![],
                     ideals: vec![],
@@ -130,130 +118,8 @@ mod tests {
     }
 
     #[test]
-    fn _characters_with_no_classes_should_be_level_0() {
-        let character = Character::dummy();
-
-        assert_eq!(character.get_level(), 0);
-    }
-
-    #[test]
-    fn _characters_with_only_one_class_should_be_the_classs_level() {
-        let mut character = Character::dummy();
-        character.classes = vec![Box::new(Artificer { level: 1 })];
-
-        assert_eq!(character.get_level(), 1);
-
-        character.classes = vec![Box::new(Artificer { level: 20 })];
-
-        assert_eq!(character.get_level(), 20);
-    }
-
-    #[test]
-    fn _multiclass_characters_should_sum_classes_levels() {
-        let mut character = Character::dummy();
-        character.classes = vec![
-            Box::new(Artificer { level: 1 }),
-            Box::new(Wizard { level: 1 }),
-        ];
-
-        assert_eq!(character.get_level(), 2);
-    }
-
-    #[test]
-    fn _level_1_character_should_have_proficiency_bonus_of_2() {
-        let mut character = Character::dummy();
-        character.classes = vec![Box::new(Artificer { level: 1 })];
-
-        assert_eq!(character.get_proficiency_bonus(), 2);
-    }
-
-    #[test]
-    fn _proficiency_bonus_sholud_go_up_by_1_every_4_level_ups() {
-        let mut character = Character::dummy();
-
-        character.classes = vec![Box::new(Artificer { level: 4 })];
-        assert_eq!(character.get_proficiency_bonus(), 2);
-
-        character.classes = vec![Box::new(Artificer { level: 5 })];
-        assert_eq!(character.get_proficiency_bonus(), 3);
-
-        character.classes = vec![Box::new(Artificer { level: 9 })];
-        assert_eq!(character.get_proficiency_bonus(), 4);
-
-        character.classes = vec![Box::new(Artificer { level: 13 })];
-        assert_eq!(character.get_proficiency_bonus(), 5);
-
-        character.classes = vec![Box::new(Artificer { level: 17 })];
-        assert_eq!(character.get_proficiency_bonus(), 6);
-    }
-
-    #[test]
-    fn _characters_without_a_class_should_have_no_saving_throw_proficiencies() {
-        let character = Character::dummy();
-
-        assert_eq!(
-            character.get_saving_throw_proficiency(&Ability::Strength),
-            None
-        );
-        assert_eq!(
-            character.get_saving_throw_proficiency(&Ability::Dexterity),
-            None
-        );
-        assert_eq!(
-            character.get_saving_throw_proficiency(&Ability::Constitution),
-            None
-        );
-        assert_eq!(
-            character.get_saving_throw_proficiency(&Ability::Intelligence),
-            None
-        );
-        assert_eq!(
-            character.get_saving_throw_proficiency(&Ability::Wisdom),
-            None
-        );
-        assert_eq!(
-            character.get_saving_throw_proficiency(&Ability::Charisma),
-            None
-        );
-    }
-
-    #[test]
-    fn _characters_with_1_class_should_derive_their_saving_throw_proficiencies_from_it() {
-        let mut character = Character::dummy();
-        character.classes = vec![Box::new(Artificer { level: 1 })];
-
-        assert_eq!(
-            character.get_saving_throw_proficiency(&Ability::Constitution),
-            Some(Proficiency::Proficiency)
-        );
-        assert_eq!(
-            character.get_saving_throw_proficiency(&Ability::Intelligence),
-            Some(Proficiency::Proficiency)
-        );
-    }
-
-    #[test]
-    fn _multiclass_characters_should_only_inherit_proficiencies_from_first_class() {
-        let mut multiclass_character = Character::dummy();
-        multiclass_character.classes = vec![
-            Box::new(Wizard { level: 1 }),
-            Box::new(Artificer { level: 1 }),
-        ];
-
-        assert_eq!(
-            multiclass_character.get_saving_throw_proficiency(&Ability::Wisdom),
-            Some(Proficiency::Proficiency)
-        );
-        assert_eq!(
-            multiclass_character.get_saving_throw_proficiency(&Ability::Constitution),
-            None
-        );
-    }
-
-    #[test]
     fn _should_get_saving_throw_mod_without_proficiency() {
-        let mut character = Character::dummy();
-        character.classes = vec![Box::new(Artificer { level: 1 })];
+        let character = Character::dummy();
 
         assert_eq!(character.get_saving_throw_mod(&Ability::Strength), -1);
     }
@@ -261,7 +127,9 @@ mod tests {
     #[test]
     fn _should_get_saving_throw_mod_including_proficiency_bonus() {
         let mut character = Character::dummy();
-        character.classes = vec![Box::new(Artificer { level: 1 })];
+        character
+            .classes
+            .add_class(Box::new(Artificer { level: 1 }));
 
         assert_eq!(character.get_saving_throw_mod(&Ability::Constitution), 1);
     }
--- src/class.rs
+++ src/class.rs
@@ -6,6 +6,43 @@ pub trait Class {
     fn get_saving_throw_proficiency(&self, ability: &Ability) -> Option<Proficiency>;
 }
 
+pub struct Classes(Vec<Box<dyn Class>>);
+
+impl Classes {
+    pub fn empty() -> Self {
+        Classes(vec![])
+    }
+
+    pub fn add_class(&mut self, class: Box<dyn Class>) {
+        self.0.push(class);
+    }
+
+    pub fn get_level(&self) -> usize {
+        self.0.iter().fold(0, |acc, class| acc + class.get_level())
+    }
+
+    pub fn get_saving_throw_proficiency(&self, ability: &Ability) -> Option<Proficiency> {
+        self.0
+            .first()
+            .and_then(|primary_class| primary_class.get_saving_throw_proficiency(ability))
+    }
+
+    pub fn get_proficiency_bonus(&self) -> usize {
+        self.get_level()
+            .checked_sub(1)
+            .map(|r| r / 4 + 2)
+            .unwrap_or(0)
+    }
+
+    pub fn get_saving_throw_bonus(&self, ability: &Ability) -> usize {
+        self.get_proficiency_bonus()
+            * (self
+                .get_saving_throw_proficiency(ability)
+                .map(|p| p as usize)
+                .unwrap_or(0))
+    }
+}
+
 pub struct Artificer {
     pub level: usize,
 }
@@ -37,5 +74,155 @@ impl Class for Wizard {
             Ability::Intelligence | Ability::Wisdom => Some(Proficiency::Proficiency),
             _ => None,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn _classless_should_be_level_0() {
+        assert_eq!(Classes::empty().get_level(), 0);
+    }
+
+    #[test]
+    fn _monoclasses_should_be_the_class_level() {
+        let classes = Classes(vec![Box::new(Artificer { level: 1 })]);
+
+        assert_eq!(classes.get_level(), 1);
+
+        let classes = Classes(vec![Box::new(Artificer { level: 20 })]);
+
+        assert_eq!(classes.get_level(), 20);
+    }
+
+    #[test]
+    fn _multiclasses_should_sum_classes_levels() {
+        let classes = Classes(vec![
+            Box::new(Artificer { level: 1 }),
+            Box::new(Wizard { level: 1 }),
+        ]);
+
+        assert_eq!(classes.get_level(), 2);
+    }
+
+    #[test]
+    fn _classless_should_have_0_proficiency_bonus() {
+        let classes = Classes::empty();
+
+        assert_eq!(classes.get_proficiency_bonus(), 0);
+    }
+
+    #[test]
+    fn _level_1_character_should_have_proficiency_bonus_of_2() {
+        let classes = Classes(vec![Box::new(Artificer { level: 1 })]);
+
+        assert_eq!(classes.get_proficiency_bonus(), 2);
+    }
+
+    #[test]
+    fn _proficiency_bonus_should_go_up_by_1_every_4_level_ups() {
+        let classes = Classes(vec![Box::new(Artificer { level: 4 })]);
+        assert_eq!(classes.get_proficiency_bonus(), 2);
+
+        let classes = Classes(vec![Box::new(Artificer { level: 5 })]);
+        assert_eq!(classes.get_proficiency_bonus(), 3);
+
+        let classes = Classes(vec![Box::new(Artificer { level: 9 })]);
+        assert_eq!(classes.get_proficiency_bonus(), 4);
+
+        let classes = Classes(vec![Box::new(Artificer { level: 13 })]);
+        assert_eq!(classes.get_proficiency_bonus(), 5);
+
+        let classes = Classes(vec![Box::new(Artificer { level: 17 })]);
+        assert_eq!(classes.get_proficiency_bonus(), 6);
+    }
+
+    #[test]
+    fn _classless_should_have_no_saving_throw_proficiencies() {
+        let classes = Classes::empty();
+
+        assert_eq!(
+            classes.get_saving_throw_proficiency(&Ability::Strength),
+            None
+        );
+        assert_eq!(
+            classes.get_saving_throw_proficiency(&Ability::Dexterity),
+            None
+        );
+        assert_eq!(
+            classes.get_saving_throw_proficiency(&Ability::Constitution),
+            None
+        );
+        assert_eq!(
+            classes.get_saving_throw_proficiency(&Ability::Intelligence),
+            None
+        );
+        assert_eq!(classes.get_saving_throw_proficiency(&Ability::Wisdom), None);
+        assert_eq!(
+            classes.get_saving_throw_proficiency(&Ability::Charisma),
+            None
+        );
+    }
+
+    #[test]
+    fn _monoclasses_should_derive_their_saving_throw_proficiencies_from_it() {
+        let classes = Classes(vec![Box::new(Artificer { level: 1 })]);
+
+        assert_eq!(
+            classes.get_saving_throw_proficiency(&Ability::Strength),
+            None
+        );
+        assert_eq!(
+            classes.get_saving_throw_proficiency(&Ability::Dexterity),
+            None
+        );
+        assert_eq!(
+            classes.get_saving_throw_proficiency(&Ability::Constitution),
+            Some(Proficiency::Proficiency)
+        );
+        assert_eq!(
+            classes.get_saving_throw_proficiency(&Ability::Intelligence),
+            Some(Proficiency::Proficiency)
+        );
+        assert_eq!(classes.get_saving_throw_proficiency(&Ability::Wisdom), None);
+        assert_eq!(
+            classes.get_saving_throw_proficiency(&Ability::Charisma),
+            None
+        );
+    }
+
+    #[test]
+    fn _multiclasses_should_only_inherit_proficiencies_from_first_class() {
+        let classes = Classes(vec![
+            Box::new(Wizard { level: 1 }),
+            Box::new(Artificer { level: 1 }),
+        ]);
+
+        assert_eq!(
+            classes.get_saving_throw_proficiency(&Ability::Strength),
+            None
+        );
+        assert_eq!(
+            classes.get_saving_throw_proficiency(&Ability::Dexterity),
+            None
+        );
+        assert_eq!(
+            classes.get_saving_throw_proficiency(&Ability::Constitution),
+            None
+        );
+        assert_eq!(
+            classes.get_saving_throw_proficiency(&Ability::Intelligence),
+            Some(Proficiency::Proficiency)
+        );
+        assert_eq!(
+            classes.get_saving_throw_proficiency(&Ability::Wisdom),
+            Some(Proficiency::Proficiency)
+        );
+        assert_eq!(
+            classes.get_saving_throw_proficiency(&Ability::Charisma),
+            None
+        );
     }
 }
