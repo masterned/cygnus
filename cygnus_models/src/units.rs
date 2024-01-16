9e921f114f605b334cc68e7d555861d1d5247a7c
Spencer Dent <mr.spencerdent@gmail.com>
2024-01-15 22:16:58 -0500

    feat: impl `Display` for `Duration`

--- cygnus_models/src/units.rs
+++ cygnus_models/src/units.rs
@@ -1,7 +1,24 @@
+use std::fmt;
+
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Duration {
     Instantaneous,
     Rounds(usize),
     Minutes(usize),
     Hours(usize),
+}
+
+impl fmt::Display for Duration {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Duration::Instantaneous => String::from("inst"),
+                Duration::Rounds(r) => format!("{r} rnds."),
+                Duration::Minutes(m) => format!("{m} mins."),
+                Duration::Hours(h) => format!("{h} hrs."),
+            }
+        )
+    }
 }
